f0c3b31ce7f8926dee971ca2904e41071858126f
Charlotte Söderström <charlotte@Charlottes-MacBook-Pro.local>
2026-01-17 17:08:55 +0100

    Setup pretter and adjust eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettier from 'eslint-plugin-prettier/recommended'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +16,32 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+    },
+    rules: {
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      'prettier/prettier': 'error',
+      'react/jsx-newline': ['error', { prevent: true }],
+      'react/jsx-no-useless-fragment': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ])
