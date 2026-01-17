0f4bfd90ae286431302b8a82388f9e477a52e9f7
Charlotte Söderström <charlotte@Charlottes-MacBook-Pro.local>
2026-01-17 17:29:31 +0100

    Run eslint and add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css';
-
 function App() {
   return (
-    <>
-      <h1>Pokémon App</h1>
-      <p>This is a Pokémon app</p>
-    </>
+    <div className="min-h-screen  flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">Pokémon App</h1>
+        <p className="text-gray-600">This is a Pokémon app</p>
+      </div>
+    </div>
   );
 }
 
